0bbd1c955f3a02e7a15ea5a40e49759f33deb728
mas9612 <mas9612@gmail.com>
2016-11-07 16:46:58 +0900

    Initial commit

---
+++ popup.js
@@ -0,0 +1,39 @@
+function getUrlsInCurrentWindow(callback) {
+    var queryInfo = {
+        currentWindow: true
+    };
+
+    var urls = '';
+    chrome.tabs.query(queryInfo, function (tabs) {
+        for (var i = 0; i < tabs.length; i++) {
+            urls += tabs[i].url;
+            if (i !== tabs.length - 1) {
+                urls += '\n';
+            }
+        }
+
+        callback(urls);
+    });
+
+}
+
+function copyUrlsToClipboard(urlString) {
+    var container = document.getElementById('container');
+    var textarea = document.createElement('textarea');
+
+    textarea.textContent = urlString;
+    container.appendChild(textarea);
+    textarea.select();
+    var result = document.execCommand('copy');
+    container.removeChild(textarea);
+
+    if (result) {
+        container.innerHTML = 'Copied!';
+    } else {
+        container.innerHTML = 'Copy Failed';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    getUrlsInCurrentWindow(copyUrlsToClipboard);
+});
